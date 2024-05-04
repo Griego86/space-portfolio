a6dbbf448d8ce03886a54bae94a836bd6fad81cc
Griego86 <dimi1086@hotmail.com>
2024-05-04 09:15:55 -0700

    Added CSS to skills component for upcoming icons

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -7,7 +7,9 @@ const Skills = () => {
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
     style={{transform: "scale(0.9)"}}
     >
+      <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
 
+      </div>
     </section>
   )
 }
