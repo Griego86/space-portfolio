22f77650d7d01859e6aa468824a5611c1725fdcf
Griego86 <dimi1086@hotmail.com>
2024-05-05 08:56:30 -0700

    Added project categories

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import ProjectCard from '../sub/ProjectCard'
 
-const projects = () => {
+const Projects = () => {
   return (
-    <div>projects</div>
+    <div className='flex flex-col items-center justify-center py-20'>
+      <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
+        My Projects
+      </h1>
+      <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
+        <ProjectCard 
+          src='/NextWebsite.png'
+          title='Modern Next.js Portfolio'
+          description='lopse irum'
+        />
+        <ProjectCard 
+          src='/CardImage.png'
+          title='Interactive Website Cards'
+          description='lopse irum'
+        />
+        <ProjectCard 
+          src='/SpaceWebsite.png'
+          title='Space Themed Website'
+          description='lopse irum'
+        />
+      </div>
+    </div>
   )
 }
 
-export default projects
+export default Projects 
