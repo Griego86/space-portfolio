dfe4e3cfa1fe644c1b43ba0356cdf76836ea6fe6
Griego86 <dimi1086@hotmail.com>
2024-05-05 08:40:06 -0700

    Defined interface for project-card component

---
+++ components/sub/ProjectCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  src: string;
+  title: string;
+  description: string;
+}
+
+const ProjectCard = ({src, title, description}: Props) => {
+  return (
+    <div>ProjectCard</div>
+  )
+}
+
+export default ProjectCard
